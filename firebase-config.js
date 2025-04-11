f98c1d9a4691e333ba359403d3a861c533126faa
kevink51 <kissal26@yahoo.com>
2025-04-11 03:32:09 -0400

    force: trigger rebuild after file unlock

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,16 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./config/firebase-service-account.json');
+
+const config = process.env.FIREBASE_CONFIG;
+
+const parsedConfig = JSON.parse(config.replace(/\\n/g, '\n'));
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+  credential: admin.credential.cert(parsedConfig),
+  databaseURL: `https://${parsedConfig.project_id}.firebaseio.com`
 });
 
 module.exports = admin;
+
+
+
 
