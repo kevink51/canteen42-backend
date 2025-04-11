5383b7b7c13d3aaaf163df649ca80d8eec25a44f
kevink51 <kissal26@yahoo.com>
2025-04-11 17:13:58 -0400

    Switched to local Firebase config file

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,13 @@
 const admin = require('firebase-admin');
-
-const config = process.env.FIREBASE_CONFIG;
-
-const parsedConfig = JSON.parse(config.replace(/\\n/g, '\n'));
+const serviceAccount = require('./config/firebase-service-account.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(parsedConfig),
-  databaseURL: `https://${parsedConfig.project_id}.firebaseio.com`
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
 });
 
 module.exports = admin;
+
 
 
 
