83ba3f531c33c8c336c115ed30e2913c3153c305
kevink51 <kissal26@yahoo.com>
2025-04-11 02:00:44 -0400

    refactor: switch to local Firebase JSON file

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,9 @@
 const admin = require('firebase-admin');
+const serviceAccount = require('./config/firebase-service-account.json');
 
-// Step 1: Get the raw string from the env variable
-const configString = process.env.FIREBASE_CONFIG;
-const cleanString = configString.trim().replace(/^"|"$/g, "");
-const properJson = JSON.parse(cleanString.replace(/\\n/g, "\n"));
-
-
-
-
-// Step 4: Initialize Firebase
 admin.initializeApp({
-  credential: admin.credential.cert(properJson),
-  databaseURL: `https://${properJson.project_id}.firebaseio.com`
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
 });
 
 module.exports = admin;
