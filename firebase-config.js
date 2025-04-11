4e65fe85ccd91f9abd7d1bc804b93303e9e88175
kevink51 <kissal26@yahoo.com>
2025-04-10 22:08:51 -0400

    fix: force firebase config update

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
 const admin = require('firebase-admin');
 const fs = require('fs');
 
-// Write the config string to a temporary JSON file
 const configPath = '/tmp/firebase-config.json';
 fs.writeFileSync(configPath, process.env.FIREBASE_CONFIG);
 
-// Load it as a file
 const serviceAccount = require(configPath);
 
 admin.initializeApp({
